d098fec0135d106e938be801900c675b1cca5030
Kevin Block <kevinbl@avi.com>
2019-05-05 22:51:51 -0400

    sort me you aint me

--- MergeSort/src/MergeSort.java
+++ MergeSort/src/MergeSort.java
@@ -1,50 +1,58 @@
 
 import java.util.Arrays;                                                                                               // Mc
 
-public class MergeSort 
+public class MergeSort  
 {
+    static int[] data = {1, 8, 2, 7, 6, 4, 5, 3};
+    static int[] temp = data;
+    static int[] tempMerge = new int[data.length];
+                
     public static void main(String[] args) 
     {
-        int[] data = {1, 8, 2, 7, 6, 4, 5, 3};
-        int[] temp = new int[8];
-        
-        System.out.println("ORIGINAL ARRAY: " + Arrays.toString(data));
-        System.out.println("TEMP ARRAY BEFORE MERGE: " + Arrays.toString(temp));
-        mergeSort(data, 2, 5, temp);
+        System.out.println("ORIGINAL ARRAY: " + Arrays.toString(data) + " WITH A LENGTH OF " + data.length);
+        mergeSort(0, data.length-1);
         System.out.println("TEMP ARRAY AFTER MERGE: " + Arrays.toString(temp));
         System.out.println("SORTED ARRAY AFTER TEMP IS COPIED INTO ORIG. ARRAY: "
                 + Arrays.toString(data));
     }
-    public static void mergeSort(int[] data, int from, int to, int[] temp)
+    public static void mergeSort(int from, int to)
     {
-        int mid;
+        
         if (from < to )
         {
-            mid = (from + to) / 2;
-            mergeSort(data,from,mid,temp);
-            mergeSort(data,mid+1,to,temp);
-            
-            merge(data,from,mid,to,temp);
+            int mid = (from + to) / 2;
+            mergeSort(from,mid);
+            //System.out.println("Going " + from + " to " + mid);
+            mergeSort(mid+1,to);
+            // System.out.println("Going " + (mid+1) + " to " + to);
+            merge(from,mid,to);
         }
        
     }
     
-    public static void merge(int[] data, int from, int mid, int to, int[] temp)
+    public static void merge(int from, int mid, int to)
     {
+        for(int i = from; i <= to; i++)
+        {
+            tempMerge[i] = data[i];
+        }
+        
         int left = from;
         int right = mid + 1;
         int tIndex = from;
         
         while(left <= mid && right <= to)
         {
-            if(data[left] < data[right])
+            if(tempMerge[left] < tempMerge[right])
             {
-                temp[tIndex] = data[left];
+                //System.out.println(data[left] + " less than " + data[right]);
+                temp[tIndex] = tempMerge[left];
                 left++;
             }
             else
             {
-                temp[tIndex] = data[right];
+                //System.out.println(data[left] + " greater than " + data[right]);
+                temp[tIndex] = tempMerge[right];
                 right++;
             }
          tIndex++;
@@ -53,24 +61,9 @@ public class MergeSort
         
         while (left<= mid)
         {
-            temp[tIndex] = data[left];
+            temp[tIndex] = tempMerge[left];
             left++;
             tIndex++;
         }
-        
-        
-        while (right<= to)
-        {
-            temp[tIndex] = data[right];
-            right++;
-            tIndex++;
-        }
-        
-        
-       //copy temp[from] to [to] into array\\   
-       for(int i = temp[from]; i<=temp[to];i++)
-       {
-           data[i] = temp[i];
-       }
     }
 }    
